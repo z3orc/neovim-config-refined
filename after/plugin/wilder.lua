d81316d8c3d9ab654c854d9f1bc2b60db7e753d4
z3orc <z3orc@users.noreply.github.com>
2023-01-17 23:53:52 +0100

    Lagt til wilder

---
+++ after/plugin/wilder.lua
@@ -0,0 +1,31 @@
+
+local wilder = require('wilder')
+wilder.setup({modes = {':', '/', '?'}})
+
+wilder.set_option('pipeline', {
+  wilder.branch(
+    wilder.cmdline_pipeline({
+      fuzzy = 1,
+      set_pcre2_pattern = 1,
+    }),
+    wilder.python_search_pipeline({
+      pattern = 'fuzzy',
+    })
+  ),
+})
+
+local highlighters = {
+  wilder.pcre2_highlighter(),
+  wilder.basic_highlighter(),
+}
+
+wilder.set_option('renderer', wilder.popupmenu_renderer(
+  wilder.popupmenu_border_theme({
+    highlights = {
+      border = 'Normal', -- highlight to use for the border
+    },
+    -- 'single', 'double', 'rounded' or 'solid'
+    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+    border = 'rounded',
+  })
+))
