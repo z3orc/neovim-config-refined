c52989fc059de881ee91a59d60e8705a203f70da
Peder <z3orc@users.noreply.github.com>
2023-05-03 19:38:33 +0200

    Update remap.lua

--- lua/z3orc/remap.lua
+++ lua/z3orc/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<A-'>", vim.cmd.FloatermToggle)
+
 vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
 end)
